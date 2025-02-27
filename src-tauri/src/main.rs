d3637f2ec0ab0f2333f4f0fbcb1939492847d3e0
往昔流年 <54844110+zhangsan-feng@users.noreply.github.com>
2025-02-27 13:55:53 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,24 @@ use web_service::start_web_service;
 // use web_service::utils::http_reverse_proxy::start_reverse_proxy;
 
 
+/*
+    "windows": [
+      {
+        "resizable": false,
+        "minWidth": 1600,
+        "minHeight": 700,
+        "center": true,
+        "hiddenTitle": true,
+        "contentProtected": false,
+        "maximized": false,
+        "maximizable": false,
+        "decorations": true,  是否显示头顶状态栏
+        "title": ""
+      }
+    ],
+
+*/
+
 
 #[tokio::main]
 async fn main() {
